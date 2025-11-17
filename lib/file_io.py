65689ad7d4cdda51a8e53fa4f29b997d29c4e924
kevin kiplangat <kiplangatkevin335@gmail.com>
2025-11-17 10:51:28 +0300

    completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -6,9 +6,11 @@ def write_file(file_name, file_content):
 def append_file(file_name, append_content):
     new_file_name = f"{file_name}.txt"
     with open(new_file_name, mode= "a") as file:
-        file.write("\n" + append_content) #\n for a new line in the file
+        file.write( f"{append_content}" ) #\n for a new line in the file
 
 def read_file(file_name):
     new_file_name =f"{file_name}.txt"
-    with open(new_file_name, modec ="r"):
-        file.open()
+    with open(new_file_name, mode ="r") as file:
+
+        file_content = file.read()
+        return file_content 
