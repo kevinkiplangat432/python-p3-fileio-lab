579e8040f11e2d8d47eff8176a580e2c68902452
kevin kiplangat <kiplangatkevin335@gmail.com>
2025-11-17 08:02:04 +0300

    initial commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    new_file_name = f"{file_name}.txt"
+    with open(new_file_name, mode="w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    new_file_name = f"{file_name}.txt"
+    with open(new_file_name, mode= "a") as file:
+        file.write("\n" + append_content) #\n for a new line in the file
 
 def read_file(file_name):
-    pass
+    new_file_name =f"{file_name}.txt"
+    with open(new_file_name, modec ="r"):
+        file.open()
